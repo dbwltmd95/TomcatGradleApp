50c84b10e9355e1501583c777ec389ed8f3b5327
Administrator <admin@example.com>
2023-09-04 13:04:31 +0000

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,6 @@
-
 public class Hello {
-  
-  public static String GREETING = "Hello world!";
-  
   public static void main(String []args) {
-    
+    System.out.println("Hello Test World!");    
   }  
 }
 
