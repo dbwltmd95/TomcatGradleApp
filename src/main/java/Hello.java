d671c2a2d5c12803e998db6bf451b6f2e680692b
Administrator <admin@example.com>
2023-09-05 05:32:50 +0000

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,17 @@
-
 public class Hello {
   
   public static String GREETING = "Hello world!";
   
   public static void main(String []args) {
-    
+    System.out.println(GREETING); 
+
+    while (true) {
+      try {
+        Thread.sleep(1000); // 1초 대기
+      } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+    }    
   }  
 }
 
