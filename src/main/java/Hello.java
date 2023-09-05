252e5e62c2096420d61477e59a17aeae536b18f7
Administrator <admin@example.com>
2023-09-05 05:29:10 +0000

    Revert "Update Hello.java"
    This reverts commit 64a758571e5b14a37ef718349e35d967935e3c46

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,21 +1,10 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class HelloWorldApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HelloWorldApplication.class, args);
-    }
+public class Hello {
+  
+  public static String GREETING = "Hello world!";
+  
+  public static void main(String []args) {
+    
+  }  
 }
 
-@RestController
-class HelloWorldController {
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello world jiseung!";
-    }
-}
