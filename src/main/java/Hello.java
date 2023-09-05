64a758571e5b14a37ef718349e35d967935e3c46
Administrator <admin@example.com>
2023-09-05 05:09:25 +0000

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,10 +1,21 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class Hello {
-  
-  public static String GREETING = "Hello world!";
-  
-  public static void main(String []args) {
-    
-  }  
+@SpringBootApplication
+public class HelloWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
 }
 
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello world jiseung!";
+    }
+}
